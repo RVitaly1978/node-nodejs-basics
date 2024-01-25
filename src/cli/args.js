0fda488305f06a494817e6f4acb4d7243a3f8f78
Vital Raicheu <rvitaly1978@gmail.com>
2024-01-25 21:47:02 +0300

    feat: implement cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,33 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argNamePrefix = '--';
+
+    let processedArgIndex = -1;
+    const args = [];
+    const rawArgs = process.argv.slice(2);
+
+    rawArgs.forEach((item, idx) => {
+        if (idx === processedArgIndex) { return }
+
+        processedArgIndex = idx;
+
+        if (item.startsWith(argNamePrefix)) {
+            let value = true
+            const key = item.slice(2);
+            const nextItem = rawArgs[idx + 1]
+            if (nextItem && !nextItem.startsWith(argNamePrefix)) {
+                value = nextItem;
+                processedArgIndex = idx + 1;
+            }
+            args.push({ key, value })
+        }
+    });
+
+    const formattedArgs = args.reduce((acc, { key, value }) => {
+        acc = acc ? `${acc}, ${key} is ${value}` : `${key} is ${value}`;
+        return acc;
+    }, '');
+
+    console.log(formattedArgs);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const searchPrefix = 'RSS_';
+
+    const formattedEnv = Object.entries(process.env)
+        .filter(([key, value]) => key.startsWith(searchPrefix))
+        .reduce((acc, [key, value]) => {
+            acc = acc ? `${acc}; ${key}=${value}` : `${key}=${value}`;
+            return acc;
+        }, '');
+
+    console.log(formattedEnv);
 };
 
 parseEnv();
