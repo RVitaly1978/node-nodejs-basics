907a320ce808a1131855837c27630be4d51d8ba8
Vital Raicheu <rvitaly1978@gmail.com>
2024-01-26 00:05:46 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,36 @@
+import { createReadStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import { Transform } from 'node:stream';
+import { createHash } from 'node:crypto';
+import { EOL } from 'node:os';
+
+import { getPath } from '../utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePathSegments = ['files', 'fileToCalculateHashFor.txt'];
+    const filePath = getPath(import.meta.url, filePathSegments);
+
+    const transformStream = new Transform({
+        transform (chunk, encoding, callback) {
+            const transformed = chunk.toString() + EOL;
+            callback(null, transformed);
+        },
+    });
+
+    const hash = createHash('sha256');
+
+    try {
+        await stat(filePath);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    createReadStream(filePath)
+        .pipe(hash)
+        .setEncoding('hex')
+        .pipe(transformStream)
+        .pipe(process.stdout);
 };
 
 await calculateHash();
