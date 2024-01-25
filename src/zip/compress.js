65d5a5c6a792b8bed8e32f1b605da5ee95885af9
Vital Raicheu <rvitaly1978@gmail.com>
2024-01-26 01:10:28 +0300

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,32 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import zlib from 'node:zlib';
+
+import { getPath } from '../utils.js';
+
 const compress = async () => {
-    // Write your code here 
+    const srcPathSegments = ['files', 'fileToCompress.txt'];
+    const destPathSegments = ['files', 'archive.gz'];
+
+    const srcPath = getPath(import.meta.url, srcPathSegments);
+    const destPath = getPath(import.meta.url, destPathSegments);
+
+    try {
+        await stat(srcPath);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    const writableStream = createWriteStream(destPath);
+
+    const zip = zlib.createGzip();
+
+    createReadStream(srcPath)
+        .pipe(zip)
+        .pipe(writableStream)
+        .on('error', (error) => { console.error(error) })
+        .on('finish', () => { process.exit(0) });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,32 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import zlib from 'node:zlib';
+
+import { getPath } from '../utils.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const srcPathSegments = ['files', 'archive.gz'];
+    const destPathSegments = ['files', 'fileToCompress.txt'];
+
+    const srcPath = getPath(import.meta.url, srcPathSegments);
+    const destPath = getPath(import.meta.url, destPathSegments);
+
+    try {
+        await stat(srcPath);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    const writableStream = createWriteStream(destPath);
+
+    const unzip = zlib.createUnzip();
+
+    createReadStream(srcPath)
+        .pipe(unzip)
+        .pipe(writableStream)
+        .on('error', (error) => { console.error(error) })
+        .on('finish', () => { process.exit(0) });
 };
 
 await decompress();
