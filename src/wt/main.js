8fb598e4129cb3bf2d0c3e1fd1ad047d72fc701a
Vital Raicheu <rvitaly1978@gmail.com>
2024-01-28 01:42:05 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+import { getPath } from '../utils.js';
+
+const baseNumber = 10;
+
+const service = async (path, data) => {
+    return new Promise((res) => {
+        const worker = new Worker(path, { workerData: data });
+        worker.on('message', (message) => res({ status: 'resolved', data: message }));
+        worker.on('error', () => res({ status: 'error', data: null }));
+    });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPathSegments = ['worker.js'];
+    const workerPath = getPath(import.meta.url, workerPathSegments);
+
+    const cpus = os.cpus();
+
+    const services = cpus.map((item, index) => {
+        const fbNumber = baseNumber + index;
+        return service(workerPath, fbNumber);
+    });
+
+    try {
+        const fibonacci = await Promise.all(services);
+        console.log(fibonacci);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await performCalculations();
