f96c757fa2db0372a62849425fbd6fb2bc775043
Vital Raicheu <rvitaly1978@gmail.com>
2024-01-28 00:26:57 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'node:child_process';
+
+import { getPath } from '../utils.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPathSegments = ['files', 'script.js'];
+    const scriptPath = getPath(import.meta.url, scriptPathSegments);
+
+    const scriptProcess = spawn('node', [scriptPath, ...args]);
+
+    scriptProcess.on('close', () => {
+        process.exit(0);
+    });
+
+    scriptProcess.stdout.pipe(process.stdout);
+    process.stdin.pipe(scriptProcess.stdin);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([12, 44, 'qwerty']);
